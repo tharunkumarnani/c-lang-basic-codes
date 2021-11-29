6a710a7a407d94d3f1ed262afb32d55cdc2c2bd5
tharunkumarnani <82167191+tharunkumarnani@users.noreply.github.com>
2021-11-29 12:36:23 +0530

    Three or two numbers of sum.
    Hey,
    Here you input two or three numbers and get the sum of your input numbers...

---
+++ Three numbers sum.c
@@ -0,0 +1,12 @@
+//tharunkumar
+//here you input three numbers and get the sum of numbers...
+#include <stdio.h>
+
+int main()
+{
+    int a=0,b=0,c=0,sum=0;
+    printf("enter three  numbers x,y,z format:");
+    scanf("%d,%d,%d",&a,&b,&c);
+    sum=a+b+c;
+    printf("sum of %d , %d and %d is %d.",a,b,c,sum);
+}
