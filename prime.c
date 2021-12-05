18cd2e2d19297d7d6997af05bbd2fc9bb88b5ea0
tharunkumarnani <82167191+tharunkumarnani@users.noreply.github.com>
2021-12-05 15:46:56 +0530

    checking wheather prime or not.....
    hey,
    created for checking the input number prime or not......

---
+++ prime.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,num;
+    printf("enter a number to checkinng prime or not? : ");
+    scanf("%d",&num);
+    if(num%2!=0,num%3!=0)
+    printf("%d is prime.",num);
+    else
+    printf("%d is not prime.",num);
+
+    return 0;
+}
